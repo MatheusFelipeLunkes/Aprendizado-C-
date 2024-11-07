104aa2b93196f0c4677f03b4fda7c41fc8d7c11e
Matheus Lunkes <matheus.lunkes.corp@gmail.com>
2024-11-07 07:20:03 -0300

    Introdução ao C#, começando com a estrutura basica

---
+++ Fundamentos C#/Introducao.cs
@@ -0,0 +1,14 @@
+//Todo programa em C# começa com uma estrutura basica, deve conter um estrtura que inclui namespace, classe, e metodo principal (Main),
+using System; // Fazendo importações de bibliotecas
+
+
+namespace Introducao { // Definindo o Namespace
+    class  programa // Definição da classe 
+    {
+        static void Main (string[] args) // Metodo principal
+        {
+            Console.WriteLine("Hello world"); // Exibe a mensagem no Console
+            /*Em C ANSI tem-se o comando com printf("Mensagem");*/
+        }
+    }
+}
